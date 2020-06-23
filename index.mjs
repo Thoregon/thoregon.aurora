7c80a3862d19668553bc5aaf6e32a3abbaeeb71d
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-06-23 21:05:31 +0200

    aurora QR scanner

--- index.mjs
+++ index.mjs
@@ -4,6 +4,7 @@
  * @author: Bernhard Lukassen
  */
 
-export { default as AuroraTextField } from './lib/formcomponents/auroratextfield.mjs';
+export { default as AuroraTextField }   from './lib/formcomponents/auroratextfield.mjs';
+export { default as AuroraQRScanner }   from './lib/cameracomponents/auroraqrscanner.mjs';
 
 export default {}
