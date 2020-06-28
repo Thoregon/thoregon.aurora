6f7c87ac36ac5d87f8cfdfb668a93091bc19aec2
Martin Neitz <martin.neitz@b-coop.at>
2020-06-28 19:15:20 +0200

    first event structure in the formcomponents

--- themes/material/formcomponents/auroratextfield.mjs
+++ themes/material/formcomponents/auroratextfield.mjs
@@ -8,15 +8,39 @@ import ThemeBehavior            from "../../themebehavior.mjs";
 
 export default class AuroraTextField extends ThemeBehavior {
 
+    rootElement = '';
+
     attach(jar) {
         this.jar = jar;
-        this.auroraelement = this.jar.shadowRoot;
+        this.auroraelement = this.jar.container;
+        this.rootElement   = 'martin';
 
-        var elements = this.auroraelement.getElementsByClassName("aurora-text-field");
-        elements[0].addEventListener('click', () => myfunction, false);
+        //---  CLICK event for the input field wrapper  ----------------------------------------------------------------
+        var textfield = this.auroraelement.getElementsByClassName("aurora-text-field");
+        textfield[0].addEventListener('click', this.textFieldClicked, false);
+
+        //---  KEYUP event for the input field  ------------------------------------------------------------------------
+        var inputfield = this.auroraelement.querySelectorAll("input");
+        var typing     = (event) => this.inputFieldKeyup(event, this.auroraelement);
+        inputfield[0].addEventListener('keyup', typing, false);
+
+        inputfield[0].addEventListener('keyup', this.inputFieldKeyup2, false);
     }
 
-    myfunction () {
-        alert ('hab dich...');
+    textFieldClicked () {
+        this.classList.add('focused');
+        this.parentElement.querySelectorAll("label")[0].classList.add('mdc-floating-label--float-above');
+    }
+    inputFieldKeyup(event, auroraelement ) {
+        console.log(this);
+        if (this && this.value) {
+            alert("My input has a value!");
+        }
+    }
+    inputFieldKeyup2() {
+        console.log(this);
+        if (this && this.value) {
+            alert("My input has a value!");
+        }
     }
 }
