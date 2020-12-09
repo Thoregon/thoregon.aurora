3c1cf5c803c5e23b91ddf60ace3ea909b328ba94
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-12-09 21:42:58 +0100

    truCloud/aurora: prepare widgets, segments and interceptors

--- index.mjs
+++ index.mjs
@@ -10,26 +10,26 @@ import ModelObject                          from "./lib/viewmodel/delegate/model
 import ModelCollection                      from "./lib/viewmodel/delegate/modelcollection.mjs";
 
 // UI Elements
-export { default as ThemeBehavior }         from './themes/themebehavior.mjs';
-export { default as AuroraElement }         from './lib/auroraelement.mjs';
-export { default as AuroraTextField }       from './lib/formcomponents/auroratextfield.mjs';
-export { default as AuroraButton }          from './lib/formcomponents/aurorabutton.mjs';
-export { default as AuroraChatMessage }     from './lib/formcomponents/aurorachatmessage.mjs';
-
-export { default as AuroraValidationIndicator }      from './lib/indicators/auroravalidationindicator.mjs';
-
-export { default as AuroraQRScanner }       from './lib/cameracomponents/auroraqrscanner.mjs';
+export { default as ThemeBehavior }             from './themes/themebehavior.mjs';
+export { default as AuroraElement }             from './lib/auroraelement.mjs';
+export { default as AuroraTextField }           from './lib/formcomponents/auroratextfield.mjs';
+export { default as AuroraButton }              from './lib/formcomponents/aurorabutton.mjs';
+export { default as AuroraChatMessage }         from './lib/formcomponents/aurorachatmessage.mjs';
+export { default as AuroraValidationIndicator } from './lib/indicators/auroravalidationindicator.mjs';
+export { default as AuroraQRScanner }           from './lib/cameracomponents/auroraqrscanner.mjs';
 
 // App Classes
-export { default as AuroraApp }             from './lib/blueprint/auroraapp.mjs';
-export { default as ComponentBehavior }     from './lib/blueprint/componentbehavior.mjs';
+export { default as AuroraApp }                 from './lib/blueprint/auroraapp.mjs';
+export { default as AppWidget }                 from './lib/blueprint/appwidget.mjs';
+export { default as ThoregonApp }               from './lib/blueprint/thoregonapp.mjs';
+
+// Validations
+export { ValidationMethod }                     from './lib/validation/validation.mjs'
+export { ValidationMethodEmpty }                from './lib/validation/validation.mjs'
+export { ValidationMethodRequired }             from './lib/validation/validation.mjs'
 
 // Views
-export { default as ViewModel }             from './lib/viewmodel/viewmodel.mjs';
-// export { default as ViewModelBuilder }      from './lib/viewmodel/viewmodelbuilder.mjs';
-
-// Blueprint
-export { default as ThoregonApp }           from './lib/blueprint/thoregonapp.mjs';
+export { default as ViewModel }                 from './lib/viewmodel/viewmodel.mjs';
 
 // aurora service
 export default new Aurora();
