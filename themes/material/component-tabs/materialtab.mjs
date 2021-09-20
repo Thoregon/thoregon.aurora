ed95fc128821984141e88cebcd3aa268f037f433
Martin Neitz <martin.neitz@b-coop.at>
2021-09-20 11:55:25 +0200

    consolidation push

---
+++ themes/material/component-tabs/materialtab.mjs
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021.
+ */
+
+/**
+ *
+ *
+ * @author: Bernhard Lukassen
+ */
+
+import ThemeBehavior            from "../../themebehavior.mjs";
+
+import Ripple                   from '../ripple.mjs';
+
+export default class MaterialTab extends ThemeBehavior {
+
+    rootElement  = '';
+    _pos_wrapper = {};
+    _columns     = {};
+
+    attach(jar) {
+        this.jar = jar;
+        this.container = this.jar.container;
+
+        this._pos_wrapper = this.container.getBoundingClientRect();
+
+        new Ripple( this.container.querySelector('.aurora-listitem-ripple'));
+    }
+
+}
