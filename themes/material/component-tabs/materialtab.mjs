364ebb9181d51cc960fcd2801c98ebd5bb2bb4bd
Martin Neitz <martin.neitz@b-coop.at>
2021-09-27 14:56:16 +0200

    consolidation push

--- themes/material/component-tabs/materialtab.mjs
+++ themes/material/component-tabs/materialtab.mjs
@@ -24,7 +24,17 @@ export default class MaterialTab extends ThemeBehavior {
 
         this._pos_wrapper = this.container.getBoundingClientRect();
 
+
+        //---  KEYUP event for the input field  ------------------------------------------------------------------------
+ //       this.container.addEventListener('mousedown', () => this.callbackTabOnMouseDown( this ), false);
+
+
         new Ripple( this.container.querySelector('.aurora-listitem-ripple'));
     }
+
+/*    callbackTabOnMouseDown( tab ) {
+        let tablist = tab.jar.tablist;
+        tablist.tabClicked( this );
+    }*/
 
 }
--- themes/material/component-tabs/materialtablist.mjs
+++ themes/material/component-tabs/materialtablist.mjs
@@ -24,7 +24,28 @@ export default class MaterialTabList extends ThemeBehavior {
 
         this._pos_wrapper = this.container.getBoundingClientRect();
 
+  //      this.jar.addEventListener('tabadded', event => alert(event.detail));
+        this.jar.addEventListener('tabadded', (event) => this.callbackTabAdded( event, this ), false);
+
         new Ripple( this.container.querySelector('.aurora-listitem-ripple'));
     }
 
+    callbackTabAdded(event, tablist) {
+        event.detail.addEventListener('mousedown', () => this.callbackTabOnMouseDown( this, event.detail ), false);
+    }
+    callbackTabOnMouseDown( tablist, tab ) {
+        let tabindicator   = tablist.container.querySelector('.aurora-tab-list-indicator');
+        let tablistwrapper = tablist.container.querySelector('.aurora-tab-list');
+
+        let tabstyle       = getComputedStyle(tablistwrapper);
+        let margintop      = parseInt(tabstyle.marginTop);
+
+        let left  = tab.offsetLeft;
+        let width = tab.offsetWidth;
+        let height = tab.offsetHeight;
+
+        tabindicator.style.top = height + margintop + 'px';
+        tabindicator.style.marginLeft = left + 'px';
+        tabindicator.style.width = width + 'px';
+    }
 }
