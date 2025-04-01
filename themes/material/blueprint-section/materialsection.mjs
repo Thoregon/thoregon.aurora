e4ddb3f7f8621d670d80d8ed628a03c8b44ec696
Martin Neitz <martin.neitz@b-coop.at>
2025-04-01 14:16:10 +0200

    consolidation push

--- themes/material/blueprint-section/materialsection.mjs
+++ themes/material/blueprint-section/materialsection.mjs
@@ -11,6 +11,7 @@
 import ThemeBehavior            from "../../themebehavior.mjs";
 import { validationLevel }      from "../../../lib/common.mjs";
 
+
 export default class MaterialSection extends ThemeBehavior {
 
     attach(jar) {
