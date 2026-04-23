4081d914514ba2d2c3f4043a2770ad84f1abad8d
Bernhard Lukassen <git@bernhard-lukassen.com>
2026-04-23 13:31:47 +0200

    ui jst and css references for package

---
+++ templates.mjs
@@ -0,0 +1,8 @@
+
+
+// import blueprintjst from "./themes/material/blueprint-container/materialcontainer.jst?raw";
+import haraldhasejst from "./themes/material/blueprint-container/harald-hase.jst?raw";
+
+export default {
+    '/blueprint-container/harald-hase.jst': haraldhasejst,
+};
